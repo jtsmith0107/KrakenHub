7693a174640fb986e2ef6f68a5cf95c39edb5bbe
Andre Tran <andre.tran@rallyhealth.com>
2016-04-16 16:08:55 -0700

    Stripped all yeoman dependencies from package.json and gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,5 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        ts: {
-            default : {
-                src: ["**/*.ts", "!node_modules/**", '!bin/**'],
-                options: {
-                    module: 'commonjs'
-                }
-            }
-        },
-        clean: {
-            all: ['**/*.js', '**/*.js.map', '!node_modules/**', '!Gruntfile.js', '!bin/**', 'test/**/*.js', 'test/**/*.js.map']
-        },
 	    wiredep: {
             task: {
                 src: [
@@ -27,8 +16,6 @@ module.exports = function(grunt) {
         }
     });
     
-    grunt.loadNpmTasks("grunt-ts");
-    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-wiredep');
     grunt.loadNpmTasks('grunt-karma');
     
